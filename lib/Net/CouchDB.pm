f35a7fa1ad1afb2876daf1ad408d83af0b7dccd6
Michael Hendricks <michael@ndrix.org>
2008-05-09 21:59:29 -0600

    Clean up POD and remove trailing whitespace

--- lib/Net/CouchDB.pm
+++ lib/Net/CouchDB.pm
@@ -83,70 +83,52 @@ sub uri {
 __END__
 
 =head1 NAME
- 
+
 Net::CouchDB - <One line description of module's purpose>
- 
- 
+
+
 =head1 VERSION
- 
+
 This documentation refers to Net::CouchDB version 0.01
- 
- 
+
+
 =head1 SYNOPSIS
- 
+
     use Net::CouchDB;
     # Brief but working code example(s) here showing the most common usage(s)
- 
+
     # This section will be as far as many users bother reading
     # so make it as educational and exemplary as possible.
-  
-  
-=head1 DESCRIPTION
- 
-A full description of the module and its features.
-May include numerous subsections (i.e. =head2, =head3, etc.) 
- 
- 
-=head1 SUBROUTINES/METHODS 
- 
-A separate section listing the public components of the module's interface. 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module provides.
-Name the section accordingly.
- 
-In an object-oriented module, this section should begin with a sentence of the 
-form "An object of this class represents...", to give the reader a high-level
-context to help them understand the methods that are subsequently described.
  
  
 =head1 DIAGNOSTICS
- 
+
 A list of every error and warning message that the module can generate
-(even the ones that will "never happen"), with a full explanation of each 
+(even the ones that will "never happen"), with a full explanation of each
 problem, one or more likely causes, and any suggested remedies.
- 
- 
+
+
 =head1 CONFIGURATION AND ENVIRONMENT
- 
+
 Net::CouchDB requires no configuration files or environment variables.
- 
+
 =head1 DEPENDENCIES
- 
+
 A list of all the other modules that this module relies upon, including any
 restrictions on versions, and an indication whether these required modules are
 part of the standard Perl distribution, part of the module's distribution,
 or must be installed separately.
- 
- 
+
+
 =head1 INCOMPATIBILITIES
- 
+
 A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for 
-system or program resources, or due to internal limitations of Perl 
-(for example, many modules that use source code filters are mutually 
+This may be due to name conflicts in the interface, or competition for
+system or program resources, or due to internal limitations of Perl
+(for example, many modules that use source code filters are mutually
 incompatible).
- 
- 
+
+
 =head1 BUGS AND LIMITATIONS
 
 Please report any bugs or feature requests to
@@ -190,10 +172,6 @@ L<http://search.cpan.org/dist/Net-CouchDB>
 Michael Hendricks  <michael@ndrix.org>
 
 =head1 LICENSE AND COPYRIGHT
- 
+
 Copyright (c) 2008 Michael Hendricks (<michael@ndrix.org>). All rights
 reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
- 
